295831b880d5c114cd282ddfed192152448934cf
AvaLilac <257690424+AvaLilac@users.noreply.github.com>
2026-08-19 16:28:14 -0400

    Create PluginsRepo.js

---
+++ PluginsRepo.js
@@ -0,0 +1,10 @@
+{
+  "plugins": [
+    {
+      "name": "ShiftNewLine (Mobile Only)",
+      "author": "0simp#5766",
+      "link": "https://codeberg.org/0simp/FluxerStuff/raw/branch/main/canary/userscripts/ShiftNewLine.user.js",
+      "description": "Makes adding new lines to your message require holding shift when pressing enter. Otherwise the message gets sent"
+    }
+  ]
+}
