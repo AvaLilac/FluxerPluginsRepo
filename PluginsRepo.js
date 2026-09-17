9d31d2ef0f3bdcc248140588ad234bc23c32234a
AvaLilac <257690424+AvaLilac@users.noreply.github.com>
2026-09-17 02:52:25 -0400

    Add Fluxer Banners plugin to PluginsRepo.js

--- PluginsRepo.js
+++ PluginsRepo.js
@@ -5,6 +5,12 @@
       "author": "0simp#5766",
       "link": "https://codeberg.org/0simp/FluxerStuff/raw/branch/main/canary/userscripts/ShiftNewLine.user.js",
       "description": "Makes adding new lines to your message require holding shift when pressing enter. Otherwise the message gets sent"
+    },
+    {
+      "name": "Fluxer Banners",
+      "author": "AvaLilac#5176 & 0simp#5766",
+      "link": "https://codeberg.org/DPRS/FluxerBanners/raw/branch/main/FluxerBanners.js",
+      "description": "Add Banners to your Profile for free! And See other People's Uploaded Banner's Join At https://fluxer.gg/iPiUfXdy to Upload"
     }
   ]
 }
